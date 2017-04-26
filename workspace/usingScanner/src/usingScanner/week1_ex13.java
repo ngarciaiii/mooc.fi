b05724e5385d13c16c79968730347471029d3a51
ngarciaiii <nicolas.garciaiii@gmail.com>
2017-04-25 23:56:24 -0400

    complete MOOC java files

---
+++ workspace/usingScanner/src/usingScanner/week1_ex13.java
@@ -0,0 +1,36 @@
+
+//IMPORTANT!! Used NetBeans for this NHLStatistics Part 1 exercise
+//MOOC component on this exercise is unreadable by Eclipse 
+//This is tested on NetBeans and works!
+
+
+import java.util.Scanner;
+import nhlstats.NHLStatistics;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in); 
+        
+        System.out.println("Top ten by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.top(10);
+        
+        System.out.println(System.lineSeparator() + "Top ten by goals: ");
+        NHLStatistics.sortByGoals(); 
+        NHLStatistics.top(10);
+        
+        System.out.println(System.lineSeparator() + "Top 25 by penalties: ");
+        NHLStatistics.sortByPenalties(); 
+        NHLStatistics.top(25);
+        
+        System.out.println(System.lineSeparator() + "Sidney Crosby stats: ");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+        
+        System.out.println(System.lineSeparator() + "PHiladelphia Flyers stats: ");
+        NHLStatistics.teamStatistics("PHI"); 
+
+        System.out.println(System.lineSeparator() + "Anaheim Ducks Players sorted by points: ");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
+    }
+}
